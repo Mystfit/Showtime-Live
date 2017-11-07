74f57f20fc9fadec671df4d3158c2e74b9dd3f89
Byron Mallett <byronated@gmail.com>
2017-11-07 15:31:07 +1300

    Updated tests

--- tests/ableton_live_test.py
+++ tests/ableton_live_test.py
@@ -1,20 +1,19 @@
 import time
 import threading
-import showtime
+import 
+from showtime import ZstFilter, ZstComponent, AddFilter
 from showtime import Showtime as ZST
-from showtime import ZstEventCallback, ZstURI, ZstInputPlugEventCallback
 
 
-class PlugCallback(ZstInputPlugEventCallback):
-    def run(self, plug):
-        print("Plug received value {0}".format(plug.value().float_at(0)))
+class PushComponent(ZstComponent):
+    def __init__(self, name, parent):
+        ZstComponent.__init__(self, "OUTPUT", name, parent)
+        self.activate()
+        self.plug = self.create_output_plug("out", showtime.ZST_INT)
 
-
-class EventCallback(ZstEventCallback):
-    def run(self, event):
-        print("Received stage event {0} from {1}".format(
-            event.get_update_type(), event.get_first().to_char()))
-
+    def send(self, val):
+        self.plug.append_int(val)
+        self.plug.fire()
 
 class Watcher(threading.Thread):
     def __init__(self):
@@ -30,21 +29,14 @@ watch = Watcher()
 watch.start()
 
 ZST.init()
-
-stageCallback = EventCallback()
-ZST.attach_stage_event_callback(stageCallback)
 ZST.join("127.0.0.1")
 perf = ZST.create_performer("python_perf")
 
-uri_out = ZstURI.create("ableton_perf", "B-B-Delay-{7}/Delay-{8}/Feedback", "out", ZstURI.OUT_JACK)
+uri_out = ZstURI("ableton_perf/B-B-Delay-{7}/Delay-{8}/Feedback/out", ZstURI.OUT_JACK)
 uri_in = ZstURI.create("python_perf", "ins", "plug_in", ZstURI.IN_JACK)
 
 plug_in = ZST.create_input_plug(uri_in, showtime.ZST_INT)
 ZST.connect_cable(uri_in, uri_out)
-time.sleep(0.2)
-
-plug_callback = PlugCallback()
-plug_in.attach_recv_callback(plug_callback);
 time.sleep(0.2)
 
 try:
